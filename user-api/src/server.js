1a9fbf548047c1b9f25024e9d60441be3fd4da42
ahmadsamaaneh <ahmadsamaaneh1@gmail.com>
2026-02-15 20:44:49 +0200

    finalcommit

--- user-api/src/server.js
+++ user-api/src/server.js
@@ -6,21 +6,18 @@ const userRoutes = require('./routes/user.routes');
 dotenv.config();
 const app = express();
 
-// Middleware
+
 app.use(express.json());
 
-// التوجيهات
+
 app.use('/api/users', userRoutes);
 
-// رسالة ترحيبية للتأكد من عمل السيرفر
 app.get('/', (req, res) => {
     res.send('API is working ');
 });
 
-// الاتصال بـ MongoDB Local وتشغيل السيرفر
 const startServer = async () => {
     try {
-        // نستخدم 127.0.0.1 لضمان استقرار الاتصال المحلي
         const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/user-api');
         
         console.log(` MongoDB Connected: ${conn.connection.host}`);
